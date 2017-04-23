dc8441a48ac469da873f574dae676d6a2805e66a
Szczepan Barszczowski <szczepan.barszczowski@7ninjas.com>
2017-04-23 12:47:10 +0200

    Question answer finished

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
-  def gravatar
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
-  end
+
+  include HasGravatar
+
+  has_many :answers
+
 end
